14e5a57dbced3da5072373f1e208b6c6d09c9ed5
leoperezz <leo.perez.nestares@gmail.com>
2026-03-07 17:02:14 -0500

    feat: adding train scripts

---
+++ scripts/train_val.py
@@ -0,0 +1,20 @@
+from agibot.configs.train import TrainPipelineConfigAgiBot
+from agibot.train.trainer import Trainer
+from accelerate import Accelerator
+from lerobot.configs import parser
+
+@parser.wrap()
+def train(cfg: TrainPipelineConfigAgiBot, accelerator: Accelerator | None = None):
+    """
+    Main function to train LejuRobot policy.
+    
+    Args:
+        cfg: A `TrainPipelineConfigLejuRobot` object containing all training configurations.
+        accelerator: Optional Accelerator instance. If None, one will be created automatically.
+    """
+    trainer = Trainer(cfg, accelerator)
+    trainer.setup(loss_key="loss")
+    trainer.train()
+
+if __name__ == "__main__":
+    train()
